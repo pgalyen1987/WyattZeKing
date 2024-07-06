e6ee6ad7206420bbbacfa6e9332d4c98c5dd0bc3
Wyatt <WyattZeKing@gmail.com>
2024-07-06 16:41:55 -0400

    first commit

---
+++ navbar.js
@@ -0,0 +1,17 @@
+function createMenu() {
+    $("#navbar").html("<div class=\"header - wrap\">" +
+        "< div class= \"header\" >" +
+            "<div class=\"logo\"><h1>President Pat 2024</h1></div>" +
+            "<div class=\"menu\">" +
+                "<ul>" +
+                    "<li><a href=\"index.html\">Home</a></li>" +
+                    "<li><a href=\"about.html\" class=\"active\">About Me</a></li>" +
+                    "<li><a href=\"patspick.html\">Pat's Pick' </a></li>" +
+                    "<li><a href=\"donate.html\">Donate</a></li>" +
+                "</ul>" +
+            "</div>" +
+        "</div >" +
+    "</div >< !--- header - wrap-- ->");
+}
+
+
