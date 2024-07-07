8526d18c7827232566ca7957d6e7e4c634de50d0
Wyatt <WyattZeKing@gmail.com>
2024-07-07 14:39:34 -0400

    first real commit

---
+++ js/loadrobloxgame.js
@@ -0,0 +1,6 @@
+// No additional JavaScript code is required as we are using the RobloxLauncher.js library to load the game.
+
+$(document).ready(function() {
+    // Load Roblox game
+    RobloxLauncher.LoadGame("https://www.roblox.com/games/17014505593/the-accident-horror");
+});
